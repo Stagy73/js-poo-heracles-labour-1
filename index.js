d8510a1103345912e3816bbd7bbbdd72e53c700c
Nils <nils.mehlhorn.fr@gmail.com>
2023-03-28 11:55:25 +0200

    code done

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-// First Labour : Heracles vs Nemean Lion
-// use your Figher class here
+
+const { Fighter, Enemy } = require("./src/Fighter.js");
+
+const heracles = new Fighter("🧔 Heracles", 20, 6, "Fight !");
+const nemeanLion = new Enemy("🦁 Nemean Lion", 11, 13);
+
+console.log(`${heracles.name} : ${heracles.life} 💙`);
+console.log(`${nemeanLion.name} : ${nemeanLion.life} 💙`);
+
+let round = 1;
+
+while (heracles.life > 0 && nemeanLion.life > 0) {
+    console.log(`Round ${round}:`);
+    console.log(`${heracles.name} attaque ${nemeanLion.name} !`);
+    console.log(heracles.attack(nemeanLion));
+    console.log(`${nemeanLion.name} attaque ${heracles.name} !`);
+    console.log(nemeanLion.attack(heracles));
+    console.log(`${heracles.name} : ${heracles.life} 💙`);
+    console.log(`${nemeanLion.name} : ${nemeanLion.life} 💙`);
+    round++;
+}
+
+if (heracles.life > 0) {
+    console.log(`${heracles.name} a vaincu ${nemeanLion.name} !`);
+} else {
+    console.log(`${nemeanLion.name} a vaincu ${heracles.name} !`);
+}
+
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,69 @@
-/* Fighter class definition */
+const maxLife = 100;
+class Fighter {
+    constructor(name, strength, dexterity, attackName) {
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.maxLife = 100;
+        this.life = this.maxLife;
+        this.attackName = attackName;
+    }
+
+    attack(defender) {
+        const damage = this.calculateDamage(defender);
+        defender.takeDamage(damage);
+        return `${this.name} inflige ${damage} points de dégâts à ${defender.name} (${defender.life}/${defender.maxLife} PV restants)`;
+    }
+
+    calculateDamage(defender) {
+        const attackerDamage = Math.floor(Math.random() * this.strength) + 1;
+        const defenderReducedDamage = defender.dexterity <= attackerDamage ? attackerDamage - defender.dexterity : 0;
+        return defenderReducedDamage;
+    }
+
+    fight(defender) {
+        const messages = [];
+        while (this.life > 0 && defender.life > 0) {
+            messages.push(this.attack(defender));
+            if (defender.life > 0) {
+                messages.push(defender.attack(this));
+            }
+        }
+        if (this.life > 0) {
+            messages.push(`${this.name} a vaincu ${defender.name} !`);
+        } else {
+            messages.push(`${defender.name} a vaincu ${this.name} !`);
+        }
+        return messages;
+    }
+
+    takeDamage(damage) {
+        this.life = this.life - damage <= 0 ? 0 : this.life - damage;
+    }
+}
+
+class Enemy extends Fighter {
+    constructor(name, strength, dexterity) {
+        super(name, strength, dexterity, `${name} attaque !`);
+    }
+
+    chooseAttack() {
+        const random = Math.random();
+        if (random < 0.7) {
+            return this.attack;
+        } else {
+            return this.specialAttack;
+        }
+    }
+
+    specialAttack(defender) {
+        const damage = this.calculateDamage(defender) * 2;
+        defender.takeDamage(damage);
+        return `${this.name} lance une attaque spéciale sur ${defender.name} (${damage} points de dégâts)`;
+    }
+}
+
+module.exports = {
+    Fighter,
+    Enemy
+};
